21c8dde349f3862fbe14def3f7730a4334fcba5c
daveprayag <prayagdave28@gmail.com>
2024-02-16 20:13:45 +0530

    under construction 🚀

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body className={inter.className}>
-        <div className="bg-[#4761ab] absolute top-[10rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] lg:right-[-28rem] xl:right-[-15rem] 2xl:right-[-5rem]"></div>
-        <div className="bg-[#67bf7f] absolute top-[10rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
